2100d90621d7630710cdaa63a8d2ba004b598f3a
Redavoi <milese3@mail.ru>
2022-04-21 13:18:48 +0300

    Initial commit

---
+++ Singleton.py
@@ -0,0 +1,8 @@
+class Singleton(type):
+    _classes = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._classes:
+            cls._classes[cls] = super(Singleton, cls).__call__(*args,
+                                                               **kwargs)
+        return cls._classes[cls]

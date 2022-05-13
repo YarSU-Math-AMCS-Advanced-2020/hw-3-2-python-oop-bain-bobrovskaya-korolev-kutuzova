c81a2181b5d49e836aed41f3783892883de49f16
Redavoi <milese3@mail.ru>
2022-05-13 21:45:53 +0300

    Add display_categories function

--- UserInterface.py
+++ UserInterface.py
@@ -10,5 +10,15 @@ class UserInterface:
         print(self.products_db.db_cols[9], self.products_db.db_cols[1],
               self.products_db.db_cols[2])
         for note in self.products_db.get_all():
-            # Print idx, name, price
+            # Print idx, name, price, is index from
+            # ProductDBRequests.__db_cols
             print(note[9], note[1], note[2])
+
+    def display_categories(self):
+        uniq_category = set()
+        for note in self.products_db.get_all():
+            # Added category, is index from ProductDBRequests.__db_cols
+            uniq_category.add(note[5])
+        print('Categories:')
+        for category in uniq_category:
+            print(category)

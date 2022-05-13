a966ac70cfe15ed8cbf1b3d65a84a7f8381ed811
Redavoi <milese3@mail.ru>
2022-05-13 23:59:03 +0300

    Add products_by_category function, rename products -> all_products

--- Display.py
+++ Display.py
@@ -5,7 +5,7 @@ class Display:
     def __init__(self, products_db: ProductDBRequests):
         self.products_db = products_db
 
-    def products(self):
+    def all_products(self):
         # Print idx, name, price
         print(self.products_db.db_cols[9], self.products_db.db_cols[1],
               self.products_db.db_cols[2])
@@ -22,3 +22,14 @@ class Display:
         print('Categories:')
         for category in uniq_category:
             print(category)
+
+    def products_by_category(self, category: str):
+        # Print idx, name, price
+        print(self.products_db.db_cols[9], self.products_db.db_cols[1],
+              self.products_db.db_cols[2])
+        for note in self.products_db.get_all():
+            # Print idx, name, price, is index from
+            if note[5] == category:
+                # ProductDBRequests.__db_cols
+                print(note[9], note[1], note[2])
+

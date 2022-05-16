bb27ce26e0882c4500abbbd15ceb9a510587c6ac
Redavoi <milese3@mail.ru>
2022-05-16 10:51:58 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from ProductDBRequests import ProductDBRequests
+from Display import Display
+from UserInterface import UserInterface
+from AddresDBRequests import AddresDBRequests
+from SellerDBRequests import SellerDBRequests
+from CustomerDBRequests import CustomerDBRequests
+
+
+product_db = ProductDBRequests()
+addres_db = AddresDBRequests()
+seller_db = SellerDBRequests()
+customer_db = CustomerDBRequests()
+display = Display(product_db, seller_db)
+i = UserInterface(display, product_db, seller_db, customer_db, addres_db)
+i.menu()
